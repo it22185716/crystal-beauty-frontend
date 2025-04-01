d037cb73e701a662a01ffb91d75ebfacadfecaea
maneth <it22185716@my.sliit.lk>
2025-04-01 17:42:07 +0530

    add some css

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header className="w-full bg-blue-400 h-[60px] text-white flex items-center justify-between px-6 shadow-md rounded-lg">
+        <h1 className="text-2xl font-semibold">Welcome to the Store</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
