fb8c14f252e86035bfba1e3ae49667317ae81110
maneth <it22185716@my.sliit.lk>
2025-04-08 10:39:53 +0530

    user login page and admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header"; // Changed import to default
-import ProductCard from "./components/product-card";
+import AdminPage from "./pages/adminPage";
+import LoginPage from "./pages/loginPage";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-pink-200">
-    <div className="w-[500px] flex flex-col justify-center items-center  bg-gray-200 h-[500px] relative " >
-      <div className="w-[90px] h-[90px] bg-red-600" ></div>
-      <div className="w-[90px] h-[90px] bg-green-500 fixed right-[50px] bottom-[50px]"></div>
-      <div className="w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] top-[50px]"></div>
-      <div className="w-[90px] h-[90px] bg-blue-900"></div>
-    </div>
-    </div>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/*" element={<AdminPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/*" element={<h1>404 not found</h1>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
