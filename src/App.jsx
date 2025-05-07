7414c22be75e1d5375016d6012ffc6ed210ae7dc
it22185716 <it22185716@my.sliit.lk>
2025-05-07 21:52:08 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AdminPage from "./pages/adminPage";
 import LoginPage from "./pages/loginPage";
+import Testing from "./pages/testing";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
+    <Toaster position="top-center" />
       <Routes>
         <Route path="/admin/*" element={<AdminPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/testing" element={<Testing/>} />
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/*" element={<h1>404 not found</h1>}/>
       </Routes>
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,4 +1,41 @@
+import { useState } from "react"
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom"; 
+
 export default function LoginPage() {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+
+  function  handleLogin(){
+        console.log("Email:",email)
+        console.log("Password:",password)
+     
+        axios.post(import.meta.env.VITE_BACKEND_URL+"/api/user/login",{
+            email:email,
+            password:password
+        }).then(
+            (response)=>{
+                console.log("Loogin successful",response.data);
+                toast.success("Login successful");
+                localStorage.setItem("token",response.data.token);
+                
+                const user = response.data.user; 
+                if(user.role == "admin"){
+                    navigate("/admin")
+                }else{
+                    //go to the home page
+                }
+            }
+        ).catch(
+            (error)=>{
+                console.log("login failed",error.response.data);
+                toast.error(error.response.data.message||"Login failed");
+            }
+        )
+  }
 return(
     <div className ="w-full h-screen bg-[url(/loginBg.jpg)] bg-cover bg-center flex ">
         <div className=" w-[50%] h-full" >
@@ -6,9 +43,15 @@ return(
         </div>
         <div className=" w-[50%] h-full flex flex-col justify-center items-center ">
             <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center  ">
-                <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]"type="email" placeholder="Enter your E-mail"/>
-                <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]"type="password" placeholder="password"/>
-                <button className="w-[400px] h-[50px] bg-green-300 text-greay  rounded-xl m-[5px] cursor-pointer">Login </button>
+                <input  onChange={(e)=>{
+                    setEmail(e.target.value)
+                }} className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]"type="email" placeholder="Enter your E-mail"/>
+                <input onChange={
+                    (e)=>{
+                        setPassword(e.target.value)
+                    }
+                } className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]"type="password" placeholder="password"/>
+                <button onClick={handleLogin} className="w-[400px] h-[50px] bg-green-300 text-greay  rounded-xl m-[5px] cursor-pointer">Login </button>
             </div>
         </div>
         
